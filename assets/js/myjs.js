19a324ccc22578122026417add0733005f239406
Sapto Prasojo <prasojodesigner@gmail.com>
2017-05-19 17:29:14 +0700

    assets file

---
+++ assets/js/myjs.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	new WOW().init();
+
+	/* menu mobile slide */
+    // $(".nav-mobile").click(function(){
+    //     $("#headerweb .content").slideToggle();
+    // });
+
+});
+
+
+
+
+
+
